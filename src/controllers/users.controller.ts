4dbd73db55a60350dd63592bd63f4e5ec5a15b08
itsankitbhusal <ankitbhusal95@gmail.com>
2025-01-18 00:57:36 +0545

    basic crud

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -8,7 +8,6 @@ export class UserController {
   static async register(req: Request, res: Response): Promise<any> {
     try {
       const body: Partial<IUser> = req.body;
-      console.log("body: ", body);
       const { firstName, lastName, email, password } = body;
       if (!firstName || !lastName || !email || !password) {
         throw new Error("Please fill all the fields!");
@@ -30,6 +29,125 @@ export class UserController {
           email: createdUser?.email,
         })
       );
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(500).send(errorResponse(err.message));
+      }
+      return res.status(500).send(errorResponse("Something went wrong!"));
+    }
+  }
+
+  static async getAllUsers(req: Request, res: Response): Promise<any> {
+    try {
+      const allUsers = await User.find({});
+
+      const filteredAllUsers = allUsers?.map((user) => ({
+        _id: user?._id,
+        firstName: user?.firstName,
+        lastName: user?.lastName,
+        email: user?.email,
+      }));
+
+      res.status(200).send(successResponse(filteredAllUsers));
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(500).send(errorResponse(err.message));
+      }
+      return res.status(500).send(errorResponse("Something went wrong!"));
+    }
+  }
+
+  static async getByID(req: Request, res: Response): Promise<any> {
+    try {
+      const userID = req.params.id;
+
+      if (!userID) {
+        throw new Error("User id not found");
+      }
+
+      const user = await User.findById(userID);
+
+      if (user) {
+        res.status(200).send(
+          successResponse({
+            _id: user._id,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email,
+          })
+        );
+      } else {
+        res.status(404).send(errorResponse("User not found"));
+      }
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(500).send(errorResponse(err.message));
+      }
+      return res.status(500).send(errorResponse("Something went wrong!"));
+    }
+  }
+
+  static async update(req: Request, res: Response): Promise<any> {
+    try {
+      const userID = req.params.id;
+      const { firstName, lastName } = req.body;
+
+      if (!userID) {
+        throw new Error("User id not found");
+      }
+
+      if (!firstName || !lastName) {
+        return res.status(400).send(errorResponse("All fields are required"));
+      }
+
+      const updatedUser = await User.findByIdAndUpdate(
+        userID,
+        { firstName, lastName },
+        { new: true }
+      );
+
+      if (!updatedUser) {
+        return res.status(404).send(errorResponse("User not found"));
+      }
+
+      return res.status(200).send(
+        successResponse({
+          _id: updatedUser._id,
+          firstName: updatedUser.firstName,
+          lastName: updatedUser.lastName,
+          email: updatedUser.email,
+        })
+      );
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(500).send(errorResponse(err.message));
+      }
+      return res.status(500).send(errorResponse("Something went wrong!"));
+    }
+  }
+
+  static async delete(req: Request, res: Response): Promise<any> {
+    try {
+      const userID = req.params.id;
+
+      if (!userID) {
+        throw new Error("User id not found");
+      }
+
+      const user = await User.findByIdAndDelete(userID);
+
+      if (user) {
+        res.status(200).send(
+          successResponse({
+            _id: user._id,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email,
+          })
+        );
+      } else {
+        res.status(404).send(errorResponse("User not found"));
+      }
     } catch (err) {
       if (err instanceof Error) {
         return res.status(500).send(errorResponse(err.message));
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -4,5 +4,10 @@ import { UserController } from "../controllers/users.controller";
 const router = Router();
 
 router.post("/", UserController.register);
+router.get("/", UserController.getAllUsers);
+router.get("/:id", UserController.getByID);
+router.patch("/:id", UserController.update);
+router.delete("/:id", UserController.delete);
+
 
 export default router;
